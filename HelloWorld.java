7fc7a54938ff618a0cfa70cf493a0d7ab71f9067
IvyAutumn <gaoqian9588@126.com>
2016-09-27 16:54:28 +0800

    Add HelloWorld script by php

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Gao on 2016/9/27.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("HelloWorld");
+    }
+}
+
+class Test{
+
+}
